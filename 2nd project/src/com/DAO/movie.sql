58b27e8faf2b4f9d767ca8c03ac4e1647c472711
SMT058 <SMT058@125.136.92.214>
2020-09-16 17:11:26 +0900

    상영장 그림파일 및 디비

--- 2nd project/src/com/DAO/movie.sql
+++ 2nd project/src/com/DAO/movie.sql
@@ -1,6 +1,20 @@
 drop table movie;
+drop table screen;
+drop sequence movie_seq;
+drop sequence screen_seq;
+
 -- 테이블 생성
 create table movie(
+movie_no number,
+movieNm varchar2(100),
+director varchar2(50),
+genre varchar2(100),
+actor varchar2(150),
+openDt varchar2(50),
+img varchar2(500)
+);
+
+create table screen(
 movie_no number,
 movieNm varchar2(100),
 director varchar2(50),
@@ -27,5 +41,3 @@ CREATE SEQUENCE screen_seq
 START WITH 1
 INCREMENT BY 1;
 
-
-drop sequence seq_bbs_no;
