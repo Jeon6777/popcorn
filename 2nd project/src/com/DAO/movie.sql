871e546ff726e5023296fbcb4805f17dd2b97ca5
SMT058 <SMT058@125.136.92.214>
2020-09-16 17:29:36 +0900

    sql 문  수정

--- 2nd project/src/com/DAO/movie.sql
+++ 2nd project/src/com/DAO/movie.sql
@@ -1,11 +1,13 @@
 drop table movie;
 drop table screen;
+drop table movie_member;
+drop table movie_grade;
 drop sequence movie_seq;
 drop sequence screen_seq;
 
 -- 테이블 생성
 create table movie(
-movie_no number,
+movie_no number primary key,
 movieNm varchar2(100),
 director varchar2(50),
 genre varchar2(100),
@@ -15,7 +17,7 @@ img varchar2(500)
 );
 
 create table screen(
-movie_no number,
+movie_no number primary key,
 movieNm varchar2(100),
 director varchar2(50),
 genre varchar2(100),
@@ -24,12 +26,21 @@ openDt varchar2(50),
 img varchar2(500)
 );
 
+create table movie_member(
+id varchar2(50) primary key,
+pw varchar2(50),
+nick varchar2(50),
+genre varchar2(100)
+);
 
---상영작
-select * from screen;
+create table movie_grade(
+grade_no number primary key,
+id varchar2(50),
+movienm varchar2(100),
+grade number,
+note varchar2(200)
+);
 
---영화 DB
-select * from movie;
 
 -- 영화 시퀀스 생성
 CREATE SEQUENCE movie_seq
@@ -40,4 +51,12 @@ INCREMENT BY 1;
 CREATE SEQUENCE screen_seq
 START WITH 1
 INCREMENT BY 1;
+
+--상영작
+select * from screen;
+
+--영화 DB
+select * from movie;
+
+
 
